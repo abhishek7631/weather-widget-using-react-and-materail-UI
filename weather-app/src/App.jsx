bf45306596280e5a5942e53ca1b292330f27ce80
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-29 12:59:22 +0530

    code modified

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,10 +1,10 @@
-import Button from "@mui/material/Button";
 // import './App.css'
+import SearchBox from "./SearchBox";
 
 function App() {
   return (
     <>
-      <Button variant="text">Text</Button>
+      <SearchBox></SearchBox>
     </>
   );
 }
---
+++ weather-app/src/SearchBox.jsx
@@ -0,0 +1,39 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SearchBox.css";
+import { useState } from "react";
+
+export default function SearchBox() {
+  let [city, setCity] = useState("");
+
+  let handleChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(city);
+    setCity("");
+  };
+
+  return (
+    <div className="searchBox">
+      <h3>Search for the weather</h3>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="city"
+          label="City Name"
+          variant="outlined"
+          required
+          value={city}
+          onChange={handleChange}
+        />
+        <br></br>
+        <br></br>
+        <Button variant="contained" type="submit">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+}
