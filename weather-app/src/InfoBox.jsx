07b6f9843e53ff6acb89ff3c5e0fc1bfb6166004
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-30 18:20:14 +0530

    code modified

--- weather-app/src/InfoBox.jsx
+++ weather-app/src/InfoBox.jsx
@@ -3,20 +3,43 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import "./InfoBox.css";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 
 export default function InfoBox({ info }) {
+  const HOT_URL =
+    "https://images.unsplash.com/uploads/14121010130570e22bcdf/e1730efe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const COLD_URL =
+    "https://images.unsplash.com/photo-1668531387310-9c3c2f272d52?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const RAIN_URL =
+    "https://images.unsplash.com/photo-1613739118925-cde1e8f5d65b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
   return (
     <div className="InfoBox">
       <div className="cardContainer">
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             sx={{ height: 140 }}
-            image="https://images.unsplash.com/photo-1722858344552-7acf888a7046?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D"
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                ? HOT_URL
+                : COLD_URL
+            }
             title="green iguana"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              {info.city}
+              {info.city}{" "}
+              {info.humidity > 80 ? (
+                <ThunderstormIcon></ThunderstormIcon>
+              ) : info.temp > 15 ? (
+                <AcUnitIcon></AcUnitIcon>
+              ) : (
+                <WbSunnyIcon></WbSunnyIcon>
+              )}
             </Typography>
             <Typography
               variant="body2"
